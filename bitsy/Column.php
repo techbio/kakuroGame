55cd6fb1e4877b7ff4b428a204719fda1076568b
Curtis Moore <techbio@users.noreply.github.com>
2020-02-20 10:39:11 -0600

    Add Set subclasses for row/column

---
+++ bitsy/Column.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('Set.php');
+require_once('Cell.php');
+require_once('Digit.php');
+require_once('Combination.php');
+require_once('Permutation.php');
+
+class Column extends Set
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setIsColumn();
+    }
+
+}
+
+?>
