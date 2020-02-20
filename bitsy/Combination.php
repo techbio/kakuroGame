4f395a1419aa219434c2e5834659fa1f24b6486d
cwmoore <cwmoore@madisoncollege.edu>
2020-02-20 11:36:24 -0800

    Add Digit array to Combinations class

--- bitsy/Combination.php
+++ bitsy/Combination.php
@@ -3,17 +3,62 @@
 class Combination
 {
     private $size;
+    private $digits;
     private $permutations;
 
-    public function __construct()
+    public function __construct($args = false)
     {
-        $this->initPermutations();
+        if ($args !== false)
+        {
+            if (
+                    isset($args['size'])
+                    && $args['size'] >= 1
+                    && $args['size'] <= 9
+            )
+            {
+                $this->setSize($args['size']);
+            }
+            else
+            {
+                $this->setSize(3);
+            }
+
+            $this->initCombinations();
+            $this->initPermutations();
+        }
+    }
+
+    private function initCombinations()
+    {
+        $this->digits = [];
+        for ($i = 0; $i < $this->getSize(); $i++)
+        {
+            $this->digits[] = new Digit();
+        }
     }
 
     private function initPermutations()
     {
         $this->permutations = [];
         $this->permutations = new Permutations($this->size);
+    }
+
+    public function pruneCombinations()
+    {
+        foreach ($digits as $digit)
+        {
+
+        }
+    }
+
+    public function setSize($size)
+    {
+        $this->size = $size;        
+    }
+
+    public function getSize()
+    {
+        return $this->size;   
     }
 }
 
