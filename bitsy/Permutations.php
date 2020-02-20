71f3edbe70a3ff58fe512e40b6dc5048046f9353
cwmoore <cwmoore@madisoncollege.edu>
2020-02-20 11:35:20 -0800

    Rename Permutations class intended to handle all variations of a Combination

---
+++ bitsy/Permutations.php
@@ -0,0 +1,24 @@
+<?php
+
+class Permutations
+{
+    private $combination;
+    private $permutationsByBitmap;
+    private $permutationArrays;
+
+    public function __construct($args = false)
+    {
+        if ($args !== false)
+        {
+            $this->combination = $args['combination'];
+        }
+
+        foreach ($combination as $digit)
+        {
+
+        }
+    }
+}
+
+?>
+
