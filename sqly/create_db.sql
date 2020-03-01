30032211f8cccef5e528386f09cf5ccb9760c00f
Curtis Moore <techbio@users.noreply.github.com>
2020-02-29 21:59:51 -0600

    Solve in memory with SQL?

---
+++ sqly/create_db.sql
@@ -0,0 +1,30 @@
+# create a database of solution space
+
+#DROP DATABASE IF EXISTS kakuro;
+CREATE DATABASE kakuro;
+USE kakuro;
+
+
+CREATE TABLE grid (
+
+);
+
+CREATE TABLE puzzle (
+
+);
+
+CREATE TABLE  (
+
+);
+
+CREATE TABLE permutations (
+
+);
+
+CREATE TABLE combinations (
+
+);
+
+CREATE TABLE permutations (
+
+);
