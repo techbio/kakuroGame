22cc6fcc7137c9062a811ec232f0b4c6b2b472e6
Curtis Moore <techbio@users.noreply.github.com>
2020-03-04 22:17:29 -0600

    Replace permperms table with perms table

--- sqly/create_db.sql
+++ sqly/create_db.sql
@@ -276,122 +276,122 @@ source create_permutations.sql
 --         WHERE i.id NOT IN (a.a, a.b, a.c, a.d, a.e, a.f, a.g, a.h)
 -- );
 
-SELECT 'create permperms';
-CREATE TABLE IF NOT EXISTS permperms (
-    a TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , b TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , c TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , d TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , e TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , f TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , g TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , h TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    , i TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-    /*, permd VARCHAR(9) NOT NULL DEFAULT '0'*/
-    , permd INT UNSIGNED NOT NULL DEFAULT 0
-    , setsum TINYINT(2) UNSIGNED NOT NULL DEFAULT 0
-    , numCells TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
-) ENGINE = InnoDB;
+-- SELECT 'create permperms';
+-- CREATE TABLE IF NOT EXISTS permperms (
+--     a TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , b TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , c TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , d TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , e TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , f TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , g TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , h TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     , i TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+--     /*, permd VARCHAR(9) NOT NULL DEFAULT '0'*/
+--     , permd INT UNSIGNED NOT NULL DEFAULT 0
+--     , setsum TINYINT(2) UNSIGNED NOT NULL DEFAULT 0
+--     , numCells TINYINT(1) UNSIGNED NOT NULL DEFAULT 0
+-- ) ENGINE = InnoDB;
 
-SELECT 'populate permperms, 9 cell';
-INSERT INTO permperms (a,b,c,d,e,f,g,h,i,permd,setsum,numCells)
-    (SELECT a,b,c,d,e,f,g,h,i, perm permd, sumPermutation(perm) setsum, 9 numCells
-        FROM perms
-        GROUP BY a,b,c,d,e,f,g,h,i,perm
-        HAVING setsum=45
-        ORDER BY setsum DESC
-            , a DESC, b DESC, c DESC, d DESC
-            , e DESC, f DESC, g DESC, h DESC, i DESC
-     )
-;
-SELECT 'populate permperms, 8 cell';
-INSERT INTO permperms (a,b,c,d,e,f,g,h,permd,setsum,numCells)
-    (SELECT a,b,c,d,e,f,g,h,castVarcharLenToInt(perm, 8) permd, sumPermutation(SUBSTR(perm,1,8)) setsum, 8 numCells
-        FROM perms
-        GROUP BY a,b,c,d,e,f,g,h,SUBSTR(perm,1,8)
-        ORDER BY setsum DESC
-        , a DESC, b DESC, c DESC, d DESC
-        , e DESC, f DESC, g DESC, h DESC
-    )
-;
-SELECT 'populate permperms, 7 cell';
-INSERT INTO permperms (a,b,c,d,e,f,g,permd,setsum,numCells)
-    (SELECT a,b,c,d,e,f,g,castVarcharLenToInt(perm, 7) permd, sumPermutation(SUBSTR(perm,1,7)) setsum, 7 numCells
-        FROM perms
-        GROUP BY a,b,c,d,e,f,g,SUBSTR(perm,1,7)
-        ORDER BY setsum DESC
-            , a DESC, b DESC, c DESC, d DESC
-        , e DESC, f DESC, g DESC
-    )
-;
-SELECT 'populate permperms, 6 cell';
-INSERT INTO permperms (a,b,c,d,e,f,permd,setsum,numCells)
-    (SELECT a,b,c,d,e,f,castVarcharLenToInt(perm, 6) permd, sumPermutation(SUBSTR(perm,1,6)) setsum, 6 numCells
-        FROM perms
-        GROUP BY a,b,c,d,e,f,SUBSTR(perm,1,6)
-        ORDER BY setsum DESC
-            , a DESC, b DESC, c DESC, d DESC
-        , e DESC, f DESC
-    )
-;
-SELECT 'populate permperms, 5 cell';
-INSERT INTO permperms (a,b,c,d,e,permd,setsum,numCells)
-    (SELECT a,b,c,d,e,castVarcharLenToInt(perm, 5) permd, sumPermutation(SUBSTR(perm,1,5)) setsum, 5 numCells
-        FROM perms
-        GROUP BY a,b,c,d,e,SUBSTR(perm,1,5)
-        ORDER BY setsum DESC
-        , a DESC, b DESC, c DESC, d DESC, e DESC
-    )
-;
-SELECT 'populate permperms, 4 cell';
-INSERT INTO permperms (a,b,c,d,permd,setsum,numCells)
-    (SELECT a,b,c,d,castVarcharLenToInt(perm, 4) permd, sumPermutation(SUBSTR(perm,1,4)) setsum, 4 numCells
-        FROM perms
-        GROUP BY a,b,c,d,SUBSTR(perm,1,4)
-        ORDER BY setsum DESC
-           , a DESC, b DESC, c DESC, d DESC
-    )
-;
-SELECT 'populate permperms, 3 cell';
-INSERT INTO permperms (a,b,c,permd,setsum,numCells)
-    (SELECT a,b,c,castVarcharLenToInt(perm, 3) permd, sumPermutation(SUBSTR(perm,1,3)) setsum, 3 numCells
-        FROM perms
-        GROUP BY a,b,c,SUBSTR(perm,1,3)
-        ORDER BY setsum DESC
-            , a DESC, b DESC, c DESC
-    )
-;
-SELECT 'populate permperms, 2 cell';
-INSERT INTO permperms (a,b,permd,setsum,numCells)
-    (SELECT a,b,castVarcharLenToInt(perm, 2) permd, sumPermutation(SUBSTR(perm,1,2)), 2 numCells
-        FROM perms
-        GROUP BY a,b,SUBSTR(SUBSTR(perm,1,2),1,2)
-        ORDER BY setsum DESC
-            , a DESC, b DESC
-    )
-;
+-- SELECT 'populate permperms, 9 cell';
+-- INSERT INTO permperms (a,b,c,d,e,f,g,h,i,permd,setsum,numCells)
+--     (SELECT a,b,c,d,e,f,g,h,i, perm permd, sumPermutation(perm) setsum, 9 numCells
+--         FROM perms
+--         GROUP BY a,b,c,d,e,f,g,h,i,perm
+--         HAVING setsum=45
+--         ORDER BY setsum DESC
+--             , a DESC, b DESC, c DESC, d DESC
+--             , e DESC, f DESC, g DESC, h DESC, i DESC
+--      )
+-- ;
+-- SELECT 'populate permperms, 8 cell';
+-- INSERT INTO permperms (a,b,c,d,e,f,g,h,permd,setsum,numCells)
+--     (SELECT a,b,c,d,e,f,g,h,castVarcharLenToInt(perm, 8) permd, sumPermutation(SUBSTR(perm,1,8)) setsum, 8 numCells
+--         FROM perms
+--         GROUP BY a,b,c,d,e,f,g,h,SUBSTR(perm,1,8)
+--         ORDER BY setsum DESC
+--         , a DESC, b DESC, c DESC, d DESC
+--         , e DESC, f DESC, g DESC, h DESC
+--     )
+-- ;
+-- SELECT 'populate permperms, 7 cell';
+-- INSERT INTO permperms (a,b,c,d,e,f,g,permd,setsum,numCells)
+--     (SELECT a,b,c,d,e,f,g,castVarcharLenToInt(perm, 7) permd, sumPermutation(SUBSTR(perm,1,7)) setsum, 7 numCells
+--         FROM perms
+--         GROUP BY a,b,c,d,e,f,g,SUBSTR(perm,1,7)
+--         ORDER BY setsum DESC
+--             , a DESC, b DESC, c DESC, d DESC
+--         , e DESC, f DESC, g DESC
+--     )
+-- ;
+-- SELECT 'populate permperms, 6 cell';
+-- INSERT INTO permperms (a,b,c,d,e,f,permd,setsum,numCells)
+--     (SELECT a,b,c,d,e,f,castVarcharLenToInt(perm, 6) permd, sumPermutation(SUBSTR(perm,1,6)) setsum, 6 numCells
+--         FROM perms
+--         GROUP BY a,b,c,d,e,f,SUBSTR(perm,1,6)
+--         ORDER BY setsum DESC
+--             , a DESC, b DESC, c DESC, d DESC
+--         , e DESC, f DESC
+--     )
+-- ;
+-- SELECT 'populate permperms, 5 cell';
+-- INSERT INTO permperms (a,b,c,d,e,permd,setsum,numCells)
+--     (SELECT a,b,c,d,e,castVarcharLenToInt(perm, 5) permd, sumPermutation(SUBSTR(perm,1,5)) setsum, 5 numCells
+--         FROM perms
+--         GROUP BY a,b,c,d,e,SUBSTR(perm,1,5)
+--         ORDER BY setsum DESC
+--         , a DESC, b DESC, c DESC, d DESC, e DESC
+--     )
+-- ;
+-- SELECT 'populate permperms, 4 cell';
+-- INSERT INTO permperms (a,b,c,d,permd,setsum,numCells)
+--     (SELECT a,b,c,d,castVarcharLenToInt(perm, 4) permd, sumPermutation(SUBSTR(perm,1,4)) setsum, 4 numCells
+--         FROM perms
+--         GROUP BY a,b,c,d,SUBSTR(perm,1,4)
+--         ORDER BY setsum DESC
+--            , a DESC, b DESC, c DESC, d DESC
+--     )
+-- ;
+-- SELECT 'populate permperms, 3 cell';
+-- INSERT INTO permperms (a,b,c,permd,setsum,numCells)
+--     (SELECT a,b,c,castVarcharLenToInt(perm, 3) permd, sumPermutation(SUBSTR(perm,1,3)) setsum, 3 numCells
+--         FROM perms
+--         GROUP BY a,b,c,SUBSTR(perm,1,3)
+--         ORDER BY setsum DESC
+--             , a DESC, b DESC, c DESC
+--     )
+-- ;
+-- SELECT 'populate permperms, 2 cell';
+-- INSERT INTO permperms (a,b,permd,setsum,numCells)
+--     (SELECT a,b,castVarcharLenToInt(perm, 2) permd, sumPermutation(SUBSTR(perm,1,2)), 2 numCells
+--         FROM perms
+--         GROUP BY a,b,SUBSTR(SUBSTR(perm,1,2),1,2)
+--         ORDER BY setsum DESC
+--             , a DESC, b DESC
+--     )
+-- ;
 
-SELECT 'add cellset and bitmap columns to permperms';
-ALTER TABLE permperms ADD COLUMN cellset VARCHAR(9) NOT NULL DEFAULT '000000000';
-ALTER TABLE permperms ADD COLUMN bitmap BINARY(9) NOT NULL DEFAULT 0b000000000;
+SELECT 'add cellset and bitmap columns to perms';
+ALTER TABLE perms ADD COLUMN cellset VARCHAR(9) NOT NULL DEFAULT '000000000';
+ALTER TABLE perms ADD COLUMN bitmap BINARY(9) NOT NULL DEFAULT 0b000000000;
 
 SELECT 'get combinations from permutations';
-UPDATE permperms SET bitmap = toBitmap(permd);
-UPDATE permperms SET cellset = fromBitmap(bitmap);
+UPDATE perms SET bitmap = toBitmap(permd);
+UPDATE perms SET cellset = fromBitmap(bitmap);
 
-SELECT 'add indexes to permperms';
-ALTER TABLE permperms ADD CONSTRAINT PRIMARY KEY pk (permd);
-ALTER TABLE permperms ADD INDEX combination (cellset);
-ALTER TABLE permperms ADD INDEX bits (bitmap);
-ALTER TABLE permperms ADD INDEX ax (a);
-ALTER TABLE permperms ADD INDEX bx (b);
-ALTER TABLE permperms ADD INDEX cx (c);
-ALTER TABLE permperms ADD INDEX dx (d);
-ALTER TABLE permperms ADD INDEX ex (e);
-ALTER TABLE permperms ADD INDEX fx (f);
-ALTER TABLE permperms ADD INDEX gx (g);
-ALTER TABLE permperms ADD INDEX hx (h);
-ALTER TABLE permperms ADD INDEX ix (i);
+SELECT 'add indexes to perms';
+ALTER TABLE perms ADD CONSTRAINT PRIMARY KEY pk (permd);
+ALTER TABLE perms ADD INDEX combination (cellset);
+ALTER TABLE perms ADD INDEX bits (bitmap);
+ALTER TABLE perms ADD INDEX ax (a);
+ALTER TABLE perms ADD INDEX bx (b);
+ALTER TABLE perms ADD INDEX cx (c);
+ALTER TABLE perms ADD INDEX dx (d);
+ALTER TABLE perms ADD INDEX ex (e);
+ALTER TABLE perms ADD INDEX fx (f);
+ALTER TABLE perms ADD INDEX gx (g);
+ALTER TABLE perms ADD INDEX hx (h);
+ALTER TABLE perms ADD INDEX ix (i);
 
 -- DROP TABLE all_sets;
 -- DROP TABLE perms;
