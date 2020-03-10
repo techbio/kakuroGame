fdcaceb7142dcba02a9af6d102d676b5500d905f
cwmoore <cwmoore@madisoncollege.edu>
2020-03-09 19:44:49 -0500

    Reformat test puzzle string

--- sqly/test_puzzle.sql
+++ sqly/test_puzzle.sql
@@ -1,5 +1,12 @@
 -- given numbers are sums of row or column sets
 -- -1 is a dud
 -- 0 is an empty cell to fill in
--1,3,5;5,0,0;3,0,0
-0,3,5;5,2,3;3,1,2
+
+-- puzzle
+-1,3,5;
+5,0,0;
+3,0,0
+-- completed
+-1,3,5;
+5,2,3;
+3,1,2
