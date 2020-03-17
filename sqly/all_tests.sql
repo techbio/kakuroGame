9b721d9a307c136d72c1fa763f54c064a6317272
cwmoore <cwmoore@madisoncollege.edu>
2020-03-17 12:08:22 -0500

    Initial commit

---
+++ sqly/all_tests.sql
@@ -0,0 +1,4 @@
+USE kakuro;
+SHOW TABLES;
+SELECT COUNT(*) as count_perms FROM perms;
+#SELECT COUNT(*) as count_temp_perms FROM tmp_permutations;
---
+++ sqly/setup_all.sql
@@ -0,0 +1,3 @@
+# source reset_db.sql
+source create_db.sql
+source hydrate_memory_permutations.sql
